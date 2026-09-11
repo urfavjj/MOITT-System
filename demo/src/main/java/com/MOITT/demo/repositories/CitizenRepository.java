9b0f84e4cb7c934825c162a1de96a2f5a64d961c
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-11 23:04:10 +0400

    feat(Citizen): add citizen repository

---
+++ demo/src/main/java/com/MOITT/demo/repositories/CitizenRepository.java
@@ -0,0 +1,9 @@
+package com.MOITT.demo.repositories;
+
+import com.MOITT.demo.entities.Citizen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CitizenRepository extends JpaRepository<Citizen, Long> {
+}
