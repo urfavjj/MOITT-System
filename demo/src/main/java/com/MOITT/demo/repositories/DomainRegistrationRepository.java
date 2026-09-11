7c1b85c320c5ed782aaebc64e3c562673fa1266d
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-11 23:06:21 +0400

    feat(DomainRegistration): add domain registration repository

---
+++ demo/src/main/java/com/MOITT/demo/repositories/DomainRegistrationRepository.java
@@ -0,0 +1,9 @@
+package com.MOITT.demo.repositories;
+
+import com.MOITT.demo.entities.DomainRegistration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DomainRegistrationRepository extends JpaRepository<DomainRegistration, Long> {
+}
