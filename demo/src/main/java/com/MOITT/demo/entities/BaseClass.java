08bb93b10fb61cdf052b29fb8bbe0598327d3200
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-11 22:36:46 +0400

    feat(base): add shared base entity

---
+++ demo/src/main/java/com/MOITT/demo/entities/BaseClass.java
@@ -0,0 +1,22 @@
+package com.MOITT.demo.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseClass {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Boolean isActive;
+    private Date createdDate;
+    private Date updatedDate;
+}
