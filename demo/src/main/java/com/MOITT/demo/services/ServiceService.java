770b7b5aedc3927f3c1251bf6c325cf78fc4bf4a
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-12 00:07:22 +0400

    feat(Service): add service service

---
+++ demo/src/main/java/com/MOITT/demo/services/ServiceService.java
@@ -0,0 +1,72 @@
+package com.MOITT.demo.services;
+
+import com.MOITT.demo.repositories.ServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServiceService {
+    ServiceRepository serviceRepository;
+
+    @Autowired
+    public ServiceService(ServiceRepository serviceRepository) {
+        this.serviceRepository = serviceRepository;
+    }
+
+    //Add service
+    public Long addService(String name, String description, Double fee, Integer processingDays
+    ) {
+        Service service = new Service();
+        service.setIsActive(true);
+        service.setCreatedDate(new Date());
+        service.setName(name);
+        service.setDescription(description);
+        service.setFee(fee);
+        service.setProcessingDays(processingDays);
+        service = serviceRepository.save(service);
+        return service.getId();
+    }
+
+    //Get All service
+    public List<Service> getAllServices() {
+        return serviceRepository.getAllService();
+    }
+
+    //Get By Id service
+    public Service getById(Long id) {
+        Optional<Service> service = serviceRepository.findById(id);
+        if (service.isPresent() && service.get().getIsActive()) {
+            return service.get();
+        }
+        throw new ResourceNotFoundException(
+                "Service not found by id: " + id
+        );
+    }
+
+    //Update Service
+    public Service updatedService(Long id, String updateName, String updateDescription, Double updateFee, Integer updateProcessingDays) {
+        Service serviceToUpdate = getById(id);
+        serviceToUpdate.setUpdatedDate(new Date());
+        serviceToUpdate.setName(updateName);
+        serviceToUpdate.setDescription(updateDescription);
+        serviceToUpdate.setFee(updateFee);
+        serviceToUpdate.setProcessingDays(updateProcessingDays);
+        serviceToUpdate = serviceRepository.save(serviceToUpdate);
+        return serviceToUpdate;
+    }
+
+    //Delete service
+    public Boolean deleteById(Long id) {
+        Service deleteService = getById(id);
+        deleteService.setIsActive(false);
+        deleteService.setUpdatedDate(new Date());
+        serviceRepository.save(deleteService);
+        return true;
+    }
+
+
+}
