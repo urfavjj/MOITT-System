39e96784b16db2b122855544ead3c9314246f835
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-11 23:07:43 +0400

    feat(Milestone): add milestone repository

---
+++ demo/src/main/java/com/MOITT/demo/repositories/MilestoneRepository.java
@@ -0,0 +1,9 @@
+package com.MOITT.demo.repositories;
+
+import com.MOITT.demo.entities.Milestone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MilestoneRepository extends JpaRepository<Milestone, Long> {
+}
