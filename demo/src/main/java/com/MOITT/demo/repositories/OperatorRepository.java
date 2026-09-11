396adcb3322593845e6e454c42b2fc4690b31258
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-11 23:04:29 +0400

    feat(Operator): add operator repository

---
+++ demo/src/main/java/com/MOITT/demo/repositories/OperatorRepository.java
@@ -0,0 +1,9 @@
+package com.MOITT.demo.repositories;
+
+import com.MOITT.demo.entities.Operator;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OperatorRepository extends JpaRepository<Operator, Long> {
+}
