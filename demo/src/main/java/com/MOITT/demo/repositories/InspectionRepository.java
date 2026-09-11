7f2ea69edc2cfc513f9f6ddc7f3891a84f5decc9
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-11 23:07:02 +0400

    feat(Inspection): add inspection repository

---
+++ demo/src/main/java/com/MOITT/demo/repositories/InspectionRepository.java
@@ -0,0 +1,9 @@
+package com.MOITT.demo.repositories;
+
+import com.MOITT.demo.entities.Inspection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InspectionRepository extends JpaRepository<Inspection, Long> {
+}
