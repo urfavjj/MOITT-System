7d017424c3dd5b30010212b328e1830af74d8b09
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:43:15 +0400

    refactor(DomainRegistration): update domain registration repository

--- demo/src/main/java/com/MOITT/demo/repositories/DomainRegistrationRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/DomainRegistrationRepository.java
@@ -2,8 +2,19 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.DomainRegistration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DomainRegistrationRepository extends JpaRepository<DomainRegistration, Long> {
+    // Get all active domain registrations
+    @Query("SELECT d FROM DomainRegistration d WHERE d.isActive=true")
+    List<DomainRegistration> getAllDomainRegistration();
+
+    // Get active domain registration by id
+    @Query("SELECT d FROM DomainRegistration d WHERE d.isActive=true AND d.id=:abc")
+    DomainRegistration getById(@Param("abc") Long id);
 }
