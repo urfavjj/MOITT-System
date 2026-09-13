7527daef7ccdbd6aae4b7351c89e9d506b5a7505
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:41:26 +0400

    refactor(Citizen): update citizen repository

--- demo/src/main/java/com/MOITT/demo/repositories/CitizenRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/CitizenRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Citizen;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CitizenRepository extends JpaRepository<Citizen, Long> {
+    // Get all active citizens
+    @Query("SELECT c FROM Citizen c WHERE c.isActive=true")
+    List<Citizen> getAllCitizen();
+
+
+
+    // Get active citizen by id
+    @Query("SELECT c FROM Citizen c WHERE c.isActive=true AND c.id=:citizen")
+    Citizen getById(@Param("citizen") Long id);
 }
