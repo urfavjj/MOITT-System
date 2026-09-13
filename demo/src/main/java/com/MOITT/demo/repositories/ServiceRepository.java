b1b18606113013f7fe3b486f60fdcbe0c83d58c9
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:42:03 +0400

    refactor(Service): update service repository

--- demo/src/main/java/com/MOITT/demo/repositories/ServiceRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/ServiceRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, Long> {
+    // Get all active services
+    @Query("SELECT s FROM Service s WHERE s.isActive=true")
+    List<Service> getAllService();
+
+
+
+    // Get active service by id
+    @Query("SELECT s FROM Service s WHERE s.isActive=true AND s.id=:service")
+    Service getById(@Param("service") Long id);
 }
