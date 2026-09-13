da7482522aec4c03fcb64c5773b3b7a464289623
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:44:03 +0400

    refactor(Inspection): update inspection repository

--- demo/src/main/java/com/MOITT/demo/repositories/InspectionRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/InspectionRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Inspection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InspectionRepository extends JpaRepository<Inspection, Long> {
+    // Get all active inspections
+    @Query("SELECT i FROM Inspection i WHERE i.isActive=true")
+    List<Inspection> getAllInspection();
+
+
+
+    // Get active inspection by id
+    @Query("SELECT i FROM Inspection i WHERE i.isActive=true AND i.id=:inspection")
+    Inspection getById(@Param("inspection") Long id);
 }
