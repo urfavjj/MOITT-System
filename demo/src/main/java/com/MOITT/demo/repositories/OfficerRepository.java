465cc3db2fe9fc3a7a4f4a9d845c55cf1c657c73
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:41:08 +0400

    refactor(Officer): update officer repository

--- demo/src/main/java/com/MOITT/demo/repositories/OfficerRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/OfficerRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Officer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OfficerRepository extends JpaRepository<Officer, Long> {
+    // Get all active officers
+    @Query("SELECT o FROM Officer o WHERE o.isActive=true")
+    List<Officer> getAllOfficer();
+
+
+
+    // Get active officer by id
+    @Query("SELECT o FROM Officer o WHERE o.isActive=true AND o.id=:officer")
+    Officer getById(@Param("officer") Long id);
 }
