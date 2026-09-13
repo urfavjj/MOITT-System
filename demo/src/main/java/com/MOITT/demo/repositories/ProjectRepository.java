95f7446573695b5aa4e1835a4f762d8b157592c0
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:44:55 +0400

    refactor(Project): update project repository

--- demo/src/main/java/com/MOITT/demo/repositories/ProjectRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/ProjectRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    // Get all active projects
+    @Query("SELECT p FROM Project p WHERE p.isActive=true")
+    List<Project> getAllProject();
+
+
+
+    // Get active project by id
+    @Query("SELECT p FROM Project p WHERE p.isActive=true AND p.id=:project")
+    Project getById(@Param("project") Long id);
 }
