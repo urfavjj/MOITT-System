f64eb97f62991e6571231100561a908cb075b4bc
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:45:31 +0400

    refactor(Vendor): update vendor repository

--- demo/src/main/java/com/MOITT/demo/repositories/VendorRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/VendorRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VendorRepository extends JpaRepository<Vendor, Long> {
+    // Get all active vendors
+    @Query("SELECT v FROM Vendor v WHERE v.isActive=true")
+    List<Vendor> getAllVendor();
+
+
+
+    // Get active vendor by id
+    @Query("SELECT v FROM Vendor v WHERE v.isActive=true AND v.id=:vendor")
+    Vendor getById(@Param("vendor") Long id);
 }
