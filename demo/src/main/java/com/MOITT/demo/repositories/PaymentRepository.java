219fe6e34164bea8645a975368a09a6adaf1313a
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:42:38 +0400

    refactor(Payment): update payment repository

--- demo/src/main/java/com/MOITT/demo/repositories/PaymentRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/PaymentRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    // Get all active payments
+    @Query("SELECT p FROM Payment p WHERE p.isActive=true")
+    List<Payment> getAllPayment();
+
+
+
+    // Get active payment by id
+    @Query("SELECT p FROM Payment p WHERE p.isActive=true AND p.id=:payment")
+    Payment getById(@Param("payment") Long id);
 }
