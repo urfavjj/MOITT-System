945f37524ee8f8158bc109c0dcc9aaf62560bef2
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:45:11 +0400

    refactor(Milestone): update milestone repository

--- demo/src/main/java/com/MOITT/demo/repositories/MilestoneRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/MilestoneRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Milestone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MilestoneRepository extends JpaRepository<Milestone, Long> {
+    // Get all active milestones
+    @Query("SELECT m FROM Milestone m WHERE m.isActive=true")
+    List<Milestone> getAllMilestone();
+
+
+
+    // Get active milestone by id
+    @Query("SELECT m FROM Milestone m WHERE m.isActive=true AND m.id=:milestone")
+    Milestone getById(@Param("milestone") Long id);
 }
