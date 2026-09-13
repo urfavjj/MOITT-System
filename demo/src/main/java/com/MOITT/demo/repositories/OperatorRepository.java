ffc6e0ccd4bd4491bd4904aea65601c9f8019831
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:41:48 +0400

    refactor(Operator): update operator repository

--- demo/src/main/java/com/MOITT/demo/repositories/OperatorRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/OperatorRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Operator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OperatorRepository extends JpaRepository<Operator, Long> {
+    // Get all active operators
+    @Query("SELECT o FROM Operator o WHERE o.isActive=true")
+    List<Operator> getAllOperator();
+
+
+
+    // Get active operator by id
+    @Query("SELECT o FROM Operator o WHERE o.isActive=true AND o.id=:operator")
+    Operator getById(@Param("operator") Long id);
 }
