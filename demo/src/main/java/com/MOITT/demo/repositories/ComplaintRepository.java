91c59eb7a1fdddce47e99ea7f57b3da3e1c8d423
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:43:37 +0400

    refactor(Complaint): update complaint repository

--- demo/src/main/java/com/MOITT/demo/repositories/ComplaintRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/ComplaintRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Complaint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
+    // Get all active complaints
+    @Query("SELECT c FROM Complaint c WHERE c.isActive=true")
+    List<Complaint> getAllComplaint();
+
+
+
+    // Get active complaint by id
+    @Query("SELECT c FROM Complaint c WHERE c.isActive=true AND c.id=:complaint")
+    Complaint getById(@Param("complaint") Long id);
 }
