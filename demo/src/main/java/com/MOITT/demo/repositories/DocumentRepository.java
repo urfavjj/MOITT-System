7c424c4ca5e5a320b7a762c9f860987b323a5ff2
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:45:49 +0400

    refactor(Document): update document repository

--- demo/src/main/java/com/MOITT/demo/repositories/DocumentRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/DocumentRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+    // Get all active documents
+    @Query("SELECT d FROM Document d WHERE d.isActive=true")
+    List<Document> getAllDocument();
+
+
+
+    // Get active document by id
+    @Query("SELECT d FROM Document d WHERE d.isActive=true AND d.id=:document")
+    Document getById(@Param("document") Long id);
 }
