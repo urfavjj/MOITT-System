57c295d31347beba18790fc08e19e1af6f0ac5a4
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-13 10:42:55 +0400

    refactor(SpectrumLicense): update spectrum license repository

--- demo/src/main/java/com/MOITT/demo/repositories/SpectrumLicenseRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/SpectrumLicenseRepository.java
@@ -2,8 +2,21 @@ package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.SpectrumLicense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SpectrumLicenseRepository extends JpaRepository<SpectrumLicense, Long> {
+    // Get all active spectrum licenses
+    @Query("SELECT s FROM SpectrumLicense s WHERE s.isActive=true")
+    List<SpectrumLicense> getAllSpectrumLicense();
+
+
+
+    // Get active spectrum license by id
+    @Query("SELECT s FROM SpectrumLicense s WHERE s.isActive=true AND s.id=:SpectrumLicense")
+    SpectrumLicense getById(@Param("SpectrumLicense") Long id);
 }
