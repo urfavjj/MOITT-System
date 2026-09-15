18de067fb7e57a065776711046332ad1bfb4f49a
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-15 22:11:08 +0400

    feat(BusinessRuleException): adding BusinessRuleException.java file

---
+++ demo/src/main/java/com/MOITT/demo/exceptions/BusinessRuleException.java
@@ -0,0 +1,9 @@
+package com.MOITT.demo.exceptions;
+
+public class BusinessRuleException extends RuntimeException {
+
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+
+}
