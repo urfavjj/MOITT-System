e7da76e4c53d7231c31de53e500cd64a6c054e1f
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-15 22:03:29 +0400

    refactor(SpectrumLicenseRepository): updating SpectrumLicenseRepository.java file

--- demo/src/main/java/com/MOITT/demo/repositories/SpectrumLicenseRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/SpectrumLicenseRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -19,4 +20,7 @@ public interface SpectrumLicenseRepository extends JpaRepository<SpectrumLicense
     // Get active spectrum license by id
     @Query("SELECT s FROM SpectrumLicense s WHERE s.isActive=true AND s.id=:SpectrumLicense")
     SpectrumLicense getById(@Param("SpectrumLicense") Long id);
+
+    @Query("SELECT s FROM SpectrumLicense s WHERE s.isActive=true AND s.status='ACTIVE' AND s.expiryDate >= CURRENT_DATE AND s.expiryDate <= :date")
+    List<SpectrumLicense> getExpiringLicenses(@Param("date") Date date);
 }
