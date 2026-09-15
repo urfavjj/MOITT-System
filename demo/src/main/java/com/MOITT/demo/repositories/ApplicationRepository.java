78db273fba836707bc1e742cbe7b62c3424daf6b
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-09-15 22:00:31 +0400

    refactor(ApplicationRepository): updating ApplicationRepository.java file

--- demo/src/main/java/com/MOITT/demo/repositories/ApplicationRepository.java
+++ demo/src/main/java/com/MOITT/demo/repositories/ApplicationRepository.java
@@ -1,6 +1,7 @@
 package com.MOITT.demo.repositories;
 
 import com.MOITT.demo.entities.Application;
+import com.MOITT.demo.entities.ApplicationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -19,4 +20,12 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
     // Get active application by id
     @Query("SELECT a FROM Application a WHERE a.isActive=true AND a.id=:application")
     Application getById(@Param("application") Long id);
+
+    // Get applications by status
+    @Query("SELECT a FROM Application a WHERE a.isActive=true AND a.status=:status")
+    List<Application> getApplicationsByStatus(@Param("status") ApplicationStatus status);
+
+    // Get citizen application history
+    @Query("SELECT a FROM Application a WHERE a.isActive=true AND a.citizen.id=:citizenId")
+    List<Application> getCitizenApplicationHistory(@Param("citizenId") Long citizenId);
 }
